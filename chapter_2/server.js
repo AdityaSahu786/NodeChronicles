3bf08f7a7b52b26bade4fded8968a3c825ee999e
AdityaSahu786 <sahuadi786183@gmail.com>
2024-11-23 00:00:56 +0530

    EndPoint

--- chapter_2/server.js
+++ chapter_2/server.js
@@ -6,6 +6,11 @@ const express = require("express");
 const app = express()
 const PORT = 8383
 
+let data = {
+    name: 'James'
+
+}
+
 // HTTP VERBS && Routes (or paths)
 // The methods informs the nature of request and the route is a further 
 // subdirectory (basically we direct the request to the body of code to respond
@@ -18,7 +23,11 @@ app.get('/', (req, res) => {
 
     // this is endpoint number 1
     console.log("Yay i hit an endpoint!", req.method)
-    res.sendStatus(201)
+    res.send(`<body style="background:pink;color: "blue;">
+        <h1>DATA:</h1>
+            <p>${JSON.stringify(data)}</p>
+        </body>
+        `)
 
 })
 
@@ -28,5 +37,22 @@ app.get('/dashboard', (req, res) => {
 })
 
 // Type 2 - API endpoints (non visual)
+
+// CRUD-method - create-post read-get update-put and delete-delete
+
+app.get('/api/data', (req, res) => {
+    console.log('This one was for data')
+    res.send(data)
+})
+
+app.post('/api/data', (req, res) => {
+    // someone wants to create a user (for example when they click a sign up
+    // button)
+    // the user clicks the sign up button after entering their credentials, and 
+    // their browser is wired up to send out a network request to the server to
+    // handle that action
+    const newEntry = req.body
+    res.sendStatus(701)
+})
 
 app.listen(PORT, () => console.log(`Server has started on: ${PORT}`))
