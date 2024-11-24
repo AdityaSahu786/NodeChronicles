38cf87f7074bb836953357c488c64645a9d6fe41
AdityaSahu786 <sahuadi786183@gmail.com>
2024-11-24 18:43:23 +0530

    Middleware

--- chapter_3/src/routes/authRoutes.js
+++ chapter_3/src/routes/authRoutes.js
@@ -50,6 +50,31 @@ router.post('/login', (req, res) => {
    // so what we can to do, is again, one way encrypt the password the user
    // just entered
 
+   const { username, password } = req.body
+
+   try {
+     const getUser = db.prepare('SELECT * FROM users WHERE username = ?')
+     const user = getUser.get(username)
+
+     // if we cannot find a user associated with that username, return out
+     // from the function
+     if (!user) { return res.status(404).send({ message: "User not found "})}
+
+
+     const passwordIsValid = bcrypt.compareSync(password, user.password)
+     // if the password does not match, return out of the function
+     if (!passwordIsValid) {return res.status(401).send({message: "Invalid Password"})}
+     console.log(user)
+
+     // then we have a succesfull authentication
+     const token = jwt.sign({ id: user.id}, process.env.JWT_SECRET, {
+      expiresIn: '24h' })
+      res.json({ token })
+   } catch (err) {
+    console.log(err.message)
+    res.sendStatus(503)
+   }
+
 })
 
 
